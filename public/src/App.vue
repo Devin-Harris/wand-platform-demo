d23d3ad57da60426be1ad41096df44d325abc54b
Devin Harris <devinharris.316@gmail.com>
2021-09-10 17:38:18 -0400

    Initial Setup

---
+++ public/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style src="./app.scss" lang="scss"></style>
+<script src="./app.js"></script>
+
---
+++ public/src/views/Home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+  </div>
+</template>
+
+<script src="./home.js"></script>
+<style src="./home.scss" lang="scss"></style>
