ecf542c0e0a7e71023bfb70293c649d4c5c9d8d2
Devin-Harris <devin.harris316@gmail.com>
2021-09-11 13:52:11 -0400

    better animations and hover effect, also added in new images

--- public/src/components/grid/grid.js
+++ public/src/components/grid/grid.js
@@ -10,42 +10,71 @@ export default {
     return {
       centerCell: null,
       rimCells: [],
+      rimCellWidth: 0,
+      rimCellHeight: 0,
+      rimCellPositions: {
+        1: { x: 0, y: 0 },
+        2: { x: 1, y: 0 },
+        3: { x: 2, y: 0 },
+        4: { x: 3, y: 0 },
+        5: { x: 4, y: 0 },
+        6: { x: 4, y: 1 },
+        7: { x: 4, y: 2 },
+        8: { x: 4, y: 3 },
+        9: { x: 4, y: 4 },
+        10: { x: 3, y: 4 },
+        11: { x: 2, y: 4 },
+        12: { x: 1, y: 4 },
+        13: { x: 0, y: 4 },
+        14: { x: 0, y: 3 },
+        15: { x: 0, y: 2 },
+        16: { x: 0, y: 1 },
+      },
       rimCount: 16,
       centerCellInterval: null,
       outerCellInterval: null,
       revolutions: 0,
-      maxRevolutionCount: 1,
-      revolutionCellTrackerIndex: null
+      maxRevolutionCount: 1
     }
   },
   computed: {
     imagesNotInUse() {
       if (!this.rimCells || this.rimCells.length == 0 || this.rimCells.map(c => c.data).filter(c => c !== null).length == 0) return Images
-      return Images.filter(image => !this.rimCells.map(cell => cell.data).includes(image) && (this.centerCell.data !== image))
+      return Images.filter(image => !this.rimCells.map(cell => cell.data).includes(image.data) && (this.centerCell.data !== image.data))
     }
   },
-  created() {
+  mounted() {
+    window.addEventListener('resize', this.resizeGrid)
+
+    this.resizeGrid()
     this.randomizeCenterCell()
+    this.clearRimCells()
+    this.randomizeGrid()
+
     this.startMovingGrid()
-    this.centerCellInterval = setInterval(this.randomizeCenterCell, 15000)
+    this.startCellInterval()
   },
   destroyed() {
     clearInterval(this.outerCellInterval)
     clearInterval(this.centerCellInterval)
+    window.removeEventListener('resize', this.resizeGrid)
   },
-  async mounted() {
-    this.clearRimCells()
-    this.randomizeGrid()
-  },
-  methods: {  
-    cellClicked(cell, index) {
-      this.centerCell = cell
-      this.rimCells[index].data = this.getRandomNotUsedImage()
+  methods: {
+    outerCellClicked(cell, index) {
+      this.centerCell = { ...cell }
+      this.stopCellInterval()
+      this.startCellInterval()
+      const randomImage = this.getRandomNotUsedImage()
+      this.rimCells[index].data = randomImage.data
+      this.rimCells[index].hyperLink = randomImage.hyperLink
+    },
+    mainCellClick() {
+      window.open(this.centerCell.hyperLink)
     },
     clearRimCells() {
       let arr = []
       for (let i = 0; i < this.rimCount; i++) {
-        arr.push({ data: null })
+        arr.push({ data: null, position: this.rimCellPositions[i + 1] })
       }
       this.rimCells = [...arr]
     },
@@ -54,17 +83,28 @@ export default {
       return this.imagesNotInUse[randomIdx]
     },
     moveGridClockwise() {
-      let originalCells = [...this.rimCells]
       let clockWiseIndexOrder = [0, 1, 2, 3, 4, 6, 8, 10, 15, 14, 13, 12, 11, 9, 7, 5]
+
       for (let i = 0; i < this.rimCount; i++) {
-        let prevIndex = i == 0 ? clockWiseIndexOrder[clockWiseIndexOrder.length - 1] : clockWiseIndexOrder[i - 1]
+        let prevIndex = (i == 0) ? clockWiseIndexOrder[clockWiseIndexOrder.length - 1] : clockWiseIndexOrder[i - 1]
         let currIndex = clockWiseIndexOrder[i]
-        let nextIndex = i == clockWiseIndexOrder.length - 1 ? clockWiseIndexOrder[0] : clockWiseIndexOrder[i + 1]
 
         if (currIndex == 0 && this.rimCells[prevIndex].startingIndex == 0) this.revolutions++
-        
-        this.rimCells[currIndex] = originalCells[prevIndex]
-        this.rimCells[nextIndex] = originalCells[currIndex]
+
+        const currentPositionIndex = parseInt(Object.keys(this.rimCellPositions).find(key => {
+          const currentCellPosition = this.rimCells[currIndex].position
+          const keyPosition = this.rimCellPositions[key]
+          return (keyPosition.x == currentCellPosition.x &&
+            keyPosition.y == currentCellPosition.y)
+        }))
+        const nextPositionIndex = (currentPositionIndex) == this.rimCount ? 1 : currentPositionIndex + 1
+
+        this.rimCells[currIndex].position = this.rimCellPositions[nextPositionIndex]
+        if (nextPositionIndex === 1) {
+          const randomImage = this.getRandomNotUsedImage()
+          this.rimCells[currIndex].data = randomImage.data
+          this.rimCells[currIndex].hyperLink = randomImage.hyperLink
+        }
         this.rimCells = [...this.rimCells]
       }
     },
@@ -72,12 +112,22 @@ export default {
       let clockWiseIndexOrder = [0, 1, 2, 3, 4, 6, 8, 10, 15, 14, 13, 12, 11, 9, 7, 5]
       for (let i = 0; i < this.rimCount; i++) {
         let currIndex = clockWiseIndexOrder[i]
-        this.rimCells[currIndex] = { data: this.getRandomNotUsedImage(), startingIndex: currIndex }
+        this.rimCells[currIndex] = {
+          ...this.getRandomNotUsedImage(),
+          startingIndex: currIndex,
+          position: this.rimCellPositions[i + 1]
+        }
         this.rimCells = [...this.rimCells]
       }
     },
     randomizeCenterCell() {
-      this.centerCell = { data: this.getRandomNotUsedImage(), startingIndex: null }
+      this.centerCell = { ...this.getRandomNotUsedImage(), startingIndex: null }
+    },
+    resizeGrid() {
+      const grid = document.querySelector('.grid')
+      const bounds = grid.getBoundingClientRect()
+      this.rimCellWidth = bounds.width / 5
+      this.rimCellHeight = bounds.height / 5
     },
     startMovingGrid() {
       this.outerCellInterval = setInterval(this.moveGridClockwise, 1000)
@@ -87,7 +137,13 @@ export default {
       clearInterval(this.outerCellInterval)
       this.clearRimCells()
       this.randomizeGrid()
-      this.startMovingGrid()
+      // this.startMovingGrid()
+    },
+    startCellInterval() {
+      this.centerCellInterval = setInterval(this.randomizeCenterCell, 15000)
+    },
+    stopCellInterval() {
+      clearInterval(this.centerCellInterval)
     },
     sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms))
--- public/src/components/grid/index.vue
+++ public/src/components/grid/index.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="grid">
-    <cell v-for="(cell, index) in rimCells"
+    <cell
+      v-for="(cell, index) in rimCells"
       :key="cell.data + index"
       :cell="cell"
+      :style="{
+        width: rimCellWidth + 'px',
+        height: rimCellHeight + 'px',
+        position: 'absolute',
+        top: cell.position.y * rimCellHeight + 'px',
+        left: cell.position.x * rimCellWidth + 'px',
+      }"
       class="outer-cell"
-      @cell-clicked="cellClicked(cell, index)"
+      @cell-clicked="outerCellClicked(cell, index)"
     ></cell>
-    <cell :cell="centerCell" class="main-cell"></cell>
+    <cell
+      class="main-cell"
+      :cell="centerCell"
+      :style="{
+        width: 3 * rimCellWidth + 'px',
+        height: 3 * rimCellHeight + 'px',
+        position: 'absolute',
+        top: rimCellHeight + 'px',
+        left: rimCellWidth + 'px',
+      }"
+      @cell-clicked="mainCellClick"
+    ></cell>
   </div>
 </template>
 
