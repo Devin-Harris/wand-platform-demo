d391fb0809e880b167c6085b53473409d9376186
Devin-Harris <devin.harris316@gmail.com>
2021-09-10 22:38:49 -0400

    began building grid component on home page

---
+++ public/src/components/grid/grid.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'grid',
+  data() {
+    return {
+      centerCell: null,
+      rimCells: [],
+      rimCount: 16
+    }
+  },
+  created() {
+
+  }
+}
---
+++ public/src/components/grid/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid">
+    <div v-for="cell in rimCells" :key="cell" class="outer-cell"></div>
+    <div class="main-cell"></div>
+  </div>
+</template>
+
+<script src="./grid.js"></script>
+<style src="./grid.scss" lang="scss"></style>
